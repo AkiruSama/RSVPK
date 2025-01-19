23dec5a9e4d7330727a79a325a8025a18d3c730f
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-01-19 19:41:16 +0800

    Update WeddingEvents.tsx

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -91,7 +91,7 @@ function WeddingEvents() {
                 </div>
                 <div className="w-[20%] bg-[#424242] rounded-br-[1.25rem] flex items-center">
                   <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[4.9rem] tracking-[5px] leading-[1.2]">
-                    12/12/2012
+                    MAJLIS PEREMPUAN
                   </p>
                 </div>
               </div>
