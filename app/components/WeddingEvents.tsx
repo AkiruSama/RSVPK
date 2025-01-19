70743b036060557d235b7632d5293a29640b1ce5
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-01-19 17:45:54 +0800

    Update WeddingEvents.tsx

--- app/components/WeddingEvents.tsx
+++ app/components/WeddingEvents.tsx
@@ -21,7 +21,7 @@ function WeddingEvents() {
               <div className="flex">
                 <div className="w-[20%] bg-[#424242] rounded-bl-[1.25rem] flex items-center">
                   <p className="rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-5 lg:-translate-x-3.5 tracking-[5px]">
-                    MAJLIS KELUARGA LELAKI
+                    RUMAH LELAKI
                   </p>
                 </div>
                 <div className="w-[80%] bg-white px-4 py-6 rounded-br-[1.25rem] flex flex-col gap-5 items-baseline">
@@ -91,7 +91,7 @@ function WeddingEvents() {
                 </div>
                 <div className="w-[20%] bg-[#424242] rounded-br-[1.25rem] flex items-center">
                   <p className="-rotate-90 text-center text-white font-bold text-[1.75rem] -translate-x-[3rem] lg:-translate-x-[2.35rem] tracking-[5px]">
-                    MAJLIS KELUARGA PEREMPUAN
+                    RUMAH PEREMPUAN
                   </p>
                 </div>
               </div>
