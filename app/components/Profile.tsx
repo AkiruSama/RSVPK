07346e681a872756828d2a529ad864e70312186e
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-01-19 18:01:15 +0800

    Update Profile.tsx

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -17,10 +17,10 @@ function Profile() {
             ke Majlis Resepsi Perkahwinan putera-puteri kami.
           </p>
         </RevealWrapper>
-        <div className="grid grid-cols-2 mt-15">
+        <div className="grid grid-cols-2 mt-20">
           <div className="-rotate-90 flex flex-col justify-end lg:mr-4 -translate-y-4 lg:-translate-y-4">
             <RevealWrapper duration={1500} origin="bottom">
-              <p className="text-[0.75rem] tracking-[5px] flex gap-2.5 text-[#424242] text-center">
+              <p className="text-[0.75rem] tracking-[5px] flex gap-2.5 text-[#424242] text-center mt-4">
                 <span>PENGANTIN</span>
                 <span>PEREMPUAN</span>
               </p>
